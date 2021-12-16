77941d5ec8129f75c11b5bffb9cf3a591826e789
Andy Chan <1291473+sockbot@users.noreply.github.com>
2021-12-16 14:07:09 -0800

    implement locale plug

--- lib/milband_web/router.ex
+++ lib/milband_web/router.ex
@@ -8,6 +8,7 @@ defmodule MilbandWeb.Router do
     plug :put_root_layout, {MilbandWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug MilbandWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
