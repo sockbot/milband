2f28f5f8b6a3592ddb2fffd503e8090cead68a71
Andy Chan <1291473+sockbot@users.noreply.github.com>
2021-12-16 13:40:36 -0800

    add introspect plug to all endpoints

--- lib/milband_web/endpoint.ex
+++ lib/milband_web/endpoint.ex
@@ -46,5 +46,17 @@ defmodule MilbandWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+
+  plug :introspect
   plug MilbandWeb.Router
+
+  def introspect(conn, _opts) do
+    IO.puts("""
+    Verb: #{inspect(conn.method)}
+    Host: #{inspect(conn.host)}
+    Headers: #{inspect(conn.req_headers)}
+    """)
+
+    conn
+  end
 end
